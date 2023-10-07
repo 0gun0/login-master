033f25bcae065cf1875e45ad9436bec61870dd48
geonnyeong oh <geonnyeongoh@geonnyeongs-MacBook-Air.local>
2023-10-07 11:38:25 +0900

    파일정리, app.listen() 모듈화

--- login-lecture/app.js
+++ login-lecture/app.js
@@ -3,7 +3,6 @@
 //모듈
 const express = require('express');
 const app = express();
-const PORT = 3000;
 
 //라우팅
 app.set("views", "./views");
@@ -13,7 +12,4 @@ app.set("view engine", "ejs");
 const home = require('./routes/home');
 app.use('/', home); //use -> '미들웨어'를 등록해주는 메서드
 
-
-app.listen(PORT, function(){
-    console.log('3000번 서버 가동')
-});  
+module.exports = app;
